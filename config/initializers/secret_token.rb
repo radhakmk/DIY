809dd606acce89e87400b0cfb004f5cd638f8397
Mohan <mohan@LNGNPVV-4148912.local>
2010-12-31 22:26:47 -0500

    Do It Yourself - First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinery::Application.config.secret_token = '95aeaa9613e32650212fc9e5730500761ed2066cd7638af22202d21f8b0cb6eb0ed94e46c6c85e05eb7576463c9534950ddb05976a1e62b850ed1eff1866cbef'
